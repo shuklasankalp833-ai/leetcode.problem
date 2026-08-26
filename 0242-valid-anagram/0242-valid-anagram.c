f2245318c31e2b30242fe8bdedd2a83fbd2f0e41
Sankalp Shukla <shuklasankalp833@gmail.com>
2026-08-26 21:28:43 +0530

    Time: 0 ms (100%), Space: 9.1 MB (34.25%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.c
@@ -0,0 +1,28 @@
+#include <stdbool.h>
+#include <string.h>
+
+bool isAnagram(char* s, char* t) {
+    int lenS = strlen(s);
+    int lenT = strlen(t);
+
+    if (lenS != lenT) {
+        return false;
+    }
+
+    int count[26] = {0};
+
+    for (int i = 0; i < lenS; i++) {
+        count[s[i] - 'a']++;
+    }
+    for (int i = 0; i < lenT; i++) {
+        count[t[i] - 'a']--;
+    }
+
+    for (int i = 0; i < 26; i++) {
+        if (count[i] != 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
