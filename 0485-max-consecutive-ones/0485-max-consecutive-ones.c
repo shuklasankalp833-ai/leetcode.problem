527f5f74a5c44f3cc33142145335c949200ab92b
Sankalp Shukla <shuklasankalp833@gmail.com>
2026-08-31 17:02:14 +0530

    Time: 3 ms (16.42%), Space: 13.4 MB (25.91%) - LeetHub

---
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.c
@@ -0,0 +1,13 @@
+int findMaxConsecutiveOnes(int* nums, int numsSize) {
+    int maxCount = 0;
+    int count = 0;
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] == 1) {
+            count++;
+            if (count > maxCount) maxCount = count;
+        } else {
+            count = 0;
+        }
+    }
+    return maxCount;
+}
